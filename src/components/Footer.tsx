61a2439665d9fec325e5e03074b0b31fc585a8ed
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-10-07 12:39:05 +0200

    updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <h2>Footer</h2>
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/" target="blank">
+            <i className="fab fa-facebook-square"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/" target="blank">
+            <i className="fab fa-instagram-square"></i>
+          </a>
+        </li>
+      </ul>
+      <p>2022 . Spotifoo</p>
     </div>
   );
 }
